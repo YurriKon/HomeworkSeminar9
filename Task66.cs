c176efb26f61231cee8a336c94dea1e68555616e
yurrikon <yurrikon@gmail.com>
2022-07-09 19:33:06 +0300

    Task66 completed

---
+++ Task66.cs
@@ -0,0 +1,19 @@
+// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine();
+
+        int start = 1;
+        int finish = 7;
+        int sum = 0;
+        Console.WriteLine("Начало отрезка: " + start);
+        Console.WriteLine("Конец отрезка: " + finish);
+        while (start <= finish)
+        {
+            sum += start;
+            start++;
+        }
+        Console.WriteLine("Сумма элементов равна " + sum);
+        
+        
+        
